9a61c86d0dc947161f75960ae72871074f29f361
Rohan Tejeswar <roh123098@gmail.com>
2021-08-01 02:24:06 +0530

    added types for express

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,10 @@
-const express = require("express");
+import express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req: any, res: any) => {
+app.get("/", (req, res) => {
   res.send("Hello World from the server! ");
 });
 
